099867f215b9358fe513253fe76221bd3034ed13
Yiannis Hadjicharalambous <Yiannis.Hadjicharalambous@netbuilder.com>
2020-01-14 16:34:10 +0000

    Blueprint & config

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -2,8 +2,8 @@ import os
 
 
 class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY")
-    SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
+    SECRET_KEY = "70445619ed6df74a8deafc05daae9f18cbbc8141"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///site.db"
     MAIL_SERVER = "smtp.googlemail.com"
     MAIL_PORT = 587
     MAIL_USE_TLS = True
