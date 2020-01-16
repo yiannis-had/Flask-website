090cfc0400ba4abeab8fcd3a39fc83cb7cbf7734
Yiannis Hadjicharalambous <Hadjicharalambous.Yiannis@gmail.com>
2020-01-16 19:24:04 +0000

    Length validator

--- flaskblog/posts/forms.py
+++ flaskblog/posts/forms.py
@@ -1,9 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Length
 
 
 class PostForm(FlaskForm):
-    title = StringField("Title", validators=[DataRequired()])
-    content = TextAreaField("Content", validators=[DataRequired()])
+    title = StringField("Title", validators=[DataRequired(),Length(min=2, max=100)])
+    content = TextAreaField("Content", validators=[DataRequired(),Length(min=2, max=500)])
     submit = SubmitField("Post")
